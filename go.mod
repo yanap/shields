e0a6872588b399cbe76e4f5ce7eb312ec2369e46
masahiro.yanai <masahiro.yanai@showroom.co.jp>
2020-01-17 16:02:27 +0900

    test customcontext

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/yanap/shields
+
+go 1.13
+
+require (
+	github.com/kelseyhightower/envconfig v1.4.0 // indirect
+	github.com/labstack/echo/v4 v4.1.13 // indirect
+)
---
+++ main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"fmt"
+	"github.com/kelseyhightower/envconfig"
+	"github.com/labstack/echo/v4"
+	"log"
+)
+
+type Config struct {
+	GoPath      string `envconfig:"GOPATH"`
+	Go111Module string `envconfig:"GO111MODULE"`
+	GoEnvShell  string `envconfig:"GOENV_SHELL"`
+}
+
+type CustomContext struct {
+	echo.Context
+	Config
+}
+
+func ConfigMiddleware(config Config) echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			cc := c.(*CustomContext)
+			cc.Config = config
+			return next(cc)
+		}
+	}
+}
+
+func (c *CustomContext) Foo() {
+	println("foo")
+}
+
+func (c *CustomContext) Bar() {
+	println("bar")
+}
+
+func main() {
+	var config Config
+	err := envconfig.Process("config", &config)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	e := echo.New()
+	e.Use(func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			cc := &CustomContext{c, config}
+			return next(cc)
+		}
+	})
+	e.Use(ConfigMiddleware(config))
+	e.GET("/env", Env)
+	e.Start(":1234")
+}
+
+func Env(c echo.Context) error {
+	cc := c.(*CustomContext)
+	c.String(200, cc.GetEnv())
+	return nil
+}
+
+func (c Config) GetEnv() string {
+	return fmt.Sprintf(
+		"%s:%s:%s",
+		c.GoPath,
+		c.Go111Module,
+		c.GoEnvShell,
+	)
+}
